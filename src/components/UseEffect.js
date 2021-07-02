1481e1930284bb393d6845c059b21090e5e4ca83
DabTheMatt <71525403+DabTheMatt@users.noreply.github.com>
2021-07-02 20:15:00 +0200

    padding

--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -1,33 +1,61 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-function Hook3 () {
-    const [fruit, setFruit] = useState("yellow");
-    const [padding, setPadding] = useState(1);
-    const [fontRemSize, setFontRemSize] = useState(16);
+function Hook3() {
+  const [fruit, setFruit] = useState("yellow");
+  const [padding, setPadding] = useState(1);
+  const [fontRemSize, setFontRemSize] = useState(16);
 
-document.body.style.background = fruit;
-document.body.style.fontSize = `${fontRemSize}px`;
+  document.body.style.background = fruit;
+  document.body.style.fontSize = `${fontRemSize}px`;
 
-useEffect(()=>{
+  useEffect(() => {
     document.body.style.padding = `${padding}rem`;
     document.body.style.background = fruit;
     document.title = fruit;
-})
+  });
 
-
-
-    return (
-        <div>
-            <p>Fruit: {fruit}</p>
-            <button onClick={()=>{setFruit("Orange")}}>Change fruit</button>
-            <p>Padding: {padding}rem</p>
-            <button onClick={()=>{setPadding(padding + 1)}}>Incrase padding by 1</button>
-            <button onClick={()=>{setPadding(padding - 1)}}>Decrase padding by 1</button>
-            <p>Font size: {fontRemSize}</p>
-            <button onClick={()=>{setFontRemSize(fontRemSize + 1)}}>Incrase base font size by 10px</button>
-            <button onClick={()=>{setFontRemSize(fontRemSize - 1)}}>Decrase base font size by 10px</button>
-        </div>
-    )
+  return (
+    <div>
+      <p>Fruit: {fruit}</p>
+      <button
+        onClick={() => {
+          setFruit("Orange");
+        }}
+      >
+        Change fruit
+      </button>
+      <p>Padding: {padding}rem</p>
+      <button
+        onClick={() => {
+          setPadding(padding + 2);
+        }}
+      >
+        Incrase padding by 2
+      </button>
+      <button
+        onClick={() => {
+          setPadding(padding - 2);
+        }}
+      >
+        Decrase padding by 1
+      </button>
+      <p>Font size: {fontRemSize}</p>
+      <button
+        onClick={() => {
+          setFontRemSize(fontRemSize + 1);
+        }}
+      >
+        Incrase base font size by 10px
+      </button>
+      <button
+        onClick={() => {
+          setFontRemSize(fontRemSize - 1);
+        }}
+      >
+        Decrase base font size by 10px
+      </button>
+    </div>
+  );
 }
 
 export default Hook3;
